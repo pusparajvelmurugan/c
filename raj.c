3f1f31d3dc9da90c4cbf5ac3cc024897486394f6
pusparajvelmurugan <30748421+pusparajvelmurugan@users.noreply.github.com>
2017-08-05 16:53:32 +0530

    Create raj.c

---
+++ raj.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("enter the number:");
+scanf("%d",&a);
+if(a==0)
+{
+printf("zero");
+}
+else if(a>0)
+{
+printf("positive");
+}
+else
+{
+printf("negative");
+}
+getch();
+}
